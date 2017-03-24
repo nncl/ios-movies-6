3125a9d58b41ee2f355b49660c30fc14d0a10ea1
Usuário Convidado <Guest@PA06MICRO16.local>
2017-03-24 20:23:26 -0300

    Preparing video

--- MoviesLib/MoviesLib/MovieViewController.swift
+++ MoviesLib/MoviesLib/MovieViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import AVFoundation
+import AVKit
 
 class MovieViewController: UIViewController {
 
@@ -18,14 +20,18 @@ class MovieViewController: UIViewController {
     @IBOutlet weak var lbScore: UILabel!
     @IBOutlet weak var tvSinopsis: UITextView!
     @IBOutlet weak var lcButtonX: NSLayoutConstraint!
+    @IBOutlet weak var viTrailer: UIView!
     
     // MARK: Properties
     var movie: Movie!
-    
+    var moviePlayer: AVPlayer! // Usamos quando queremos tocar audio/video e servev para tocar tanto local quanto remoto, i.e.: by URL/MP3, etc...
+    var moviePlayerController: AVPlayerViewController! // Vai fornecer container com série de controles para conseguirmos manipular esse vídeo; poderíamos criar o nosso próprio mas esse nos ajudará
     
     // MARK: Super Methods
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        prepareVideo()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -53,10 +59,21 @@ class MovieViewController: UIViewController {
         }
     }
     
+    func prepareVideo() {
+        // Recuperar URL do vídeo
+        let url = Bundle.main.url(forResource: "logan-trailer", withExtension: "mp4")! // Sabemos que existe, então podemos desembrulha-la
+        
+        moviePlayer = AVPlayer(url: url)
+        moviePlayerController = AVPlayerViewController()
+        moviePlayerController.player = moviePlayer
+        moviePlayerController.showsPlaybackControls = true
+        moviePlayerController.view.frame = viTrailer.bounds
+        // moviePlayerController.view // View que contém o file, temos que pega-la e jogar na outra
+        viTrailer.addSubview(moviePlayerController.view)
+        
+    }
     
-    
-    
-    
-    
+    @IBAction func playVideo(_ sender: UIButton) {
+    }
     
 }
